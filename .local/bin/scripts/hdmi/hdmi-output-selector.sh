cf8ca0262d8d91f45ab2d3c41b710be214a598b4
bgautham4 <bgautham4@gmail.com>
2025-06-07 11:38:46 +0530

    Rescale bg on output change

--- .local/bin/scripts/hdmi/hdmi-output-selector.sh
+++ .local/bin/scripts/hdmi/hdmi-output-selector.sh
@@ -4,11 +4,19 @@
 
 #A dmenu based output config picker using autorandr(1)
 
+function change_and_notify {
+    autorandr -l "$1" && notify-send --urgency=low -t 5000 "Display config" "$1" 
+    #Rescale wallpaper to display
+    "$HOME"/.fehbg
+}
+
 CON=$(</sys/class/drm/card1-HDMI-A-1/status) || exit 1
-[ "$CON" = 'disconnected' ] && notify-send --urgency=low -t 5000 "Error" "No HDMI devices" && exit 0
+[ "$CON" = 'disconnected' ] && notify-send --urgency=low -t 5000 "Error" "No HDMI devices" && exit 1
 
 type=$(autorandr --list | dmenu -p "Select display config:")
-[ -n "$type" ] && autorandr -l "$type" && notify-send --urgency=low -t 5000 "Display config" "$type" || exit 0
+[ -n "$type" ] && change_and_notify "$type" || exit 0
 
 #Send signal to update brightness value
 pkill -SIGRTMIN+9 i3blocks 2> /dev/null
+
+

b79625e7f124562f905075205dbf4c9a19379817
bgautham4 <bgautham4@gmail.com>
2025-06-20 13:36:25 +0530

    Launch calcurse on click

--- .local/share/i3blocks/scripts/date.sh
+++ .local/share/i3blocks/scripts/date.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+case "$BLOCK_BUTTON" in
+    1|2|3) 
+        if command -v calcurse > /dev/null 2>&1; then
+            pgrep calcurse || i3-msg -q exec "alacritty -e calcurse"
+        fi
+    ;;
+esac
+
 date '+%Y-%m-%d(%a) %H:%M'
 #Short text
 date '+%H:%M'
